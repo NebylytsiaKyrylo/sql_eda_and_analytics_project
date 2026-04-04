fa8bde2cf2f92e48335a8f2dc833264f3e5cdc33
NebylytsiaKyrylo <knebylytsia.it@gmail.com>
2026-04-04 12:59:00 +0200

    Format SQL script for cumulative analysis to improve readability

--- scripts/08_cumulative_analysis.sql
+++ scripts/08_cumulative_analysis.sql
@@ -14,21 +14,22 @@ SQL Functions Used:
 
 -- Calculate the total sales per month 
 -- and the running total of sales over time with partition by year
-WITH sales_per_month AS (
-    SELECT
-        EXTRACT(YEAR FROM order_date) AS order_year,
-        EXTRACT(MONTH FROM order_date) AS order_month,
-        SUM(sales_amount) AS total_sales
-    FROM gold.fact_sales
-    WHERE
-        order_date IS NOT NULL
-    GROUP BY
-        order_year,
-        order_month
-    ORDER BY
-        order_year,
-        order_month
-                        )
+WITH
+    sales_per_month AS (
+        SELECT
+            EXTRACT(YEAR FROM order_date) AS order_year,
+            EXTRACT(MONTH FROM order_date) AS order_month,
+            SUM(sales_amount) AS total_sales
+        FROM gold.fact_sales
+        WHERE
+            order_date IS NOT NULL
+        GROUP BY
+            order_year,
+            order_month
+        ORDER BY
+            order_year,
+            order_month
+                       )
 SELECT
     order_year,
     order_month,
